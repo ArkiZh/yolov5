0f7222a278a2f9824085ceab1f72ba4b3f4aafea
ArkiZh <1797466468@qq.com>
2023-01-31 17:01:03 +0800

    Sure?

--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -448,9 +448,9 @@ class LoadImagesAndLabels(Dataset):
         # Read cache
         [cache.pop(k) for k in ('hash', 'version', 'msgs')]  # remove items
         labels, shapes, self.segments = zip(*cache.values())
-        nl = len(np.concatenate(labels, 0))  # number of labels
+        nl = len(labels)  # number of labels
         assert nl > 0 or not augment, f'{prefix}All labels empty in {cache_path}, can not start training. {HELP_URL}'
-        self.labels = list(labels)
+        self.labels = labels
         self.shapes = np.array(shapes)
         self.im_files = list(cache.keys())  # update
         self.label_files = img2label_paths(cache.keys())  # update
